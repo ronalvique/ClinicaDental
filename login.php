267ae352a0a1913b20d745b7b67d5caa02bb6154
ronalvique <ronal.vique@gmail.com>
2016-10-18 22:34:31 -0600

    En movil se quito el acceso al icono del menu

--- login.php
+++ login.php
@@ -38,10 +38,6 @@
 	<div class="navbar navbar-inverse">
 		<div class="navbar-header">
 			<a class="navbar-brand" href="">imagen del logo del sistema</a>
-
-			<ul class="nav navbar-nav pull-right visible-xs-block">
-				<li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
-			</ul>
 		</div>
 
 		
