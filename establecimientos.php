54055e5bc9da94c11797ebe70d0be6f876292876
germanreyes <germanreyes_18@hotmail.com>
2016-10-29 17:45:11 -0600

    test
    test

---
+++ establecimientos.php
@@ -0,0 +1,136 @@
+<?php include("includes/top_page_form.php"); ?>
+<body>
+
+		<?php include("includes/header.php"); ?>
+
+
+	<!-- Page container -->
+	<div class="page-container">
+
+		<!-- Page content -->
+		<div class="page-content">
+
+			<?php include("includes/sidebar.php"); ?>
+
+
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<!-- Page header -->
+				<div class="page-header">
+					<div class="page-header-content">
+						<div class="page-title">
+							<h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Forms</span> - Basic Inputs</h4>
+						</div>
+
+						<div class="heading-elements">
+							<div class="heading-btn-group">
+								<a href="#" class="btn btn-link btn-float has-text"><i class="icon-bars-alt text-primary"></i><span>Statistics</span></a>
+								<a href="#" class="btn btn-link btn-float has-text"><i class="icon-calculator text-primary"></i> <span>Invoices</span></a>
+								<a href="#" class="btn btn-link btn-float has-text"><i class="icon-calendar5 text-primary"></i> <span>Schedule</span></a>
+							</div>
+						</div>
+					</div>
+
+					<div class="breadcrumb-line">
+						<ul class="breadcrumb">
+							<li><a href="index.html"><i class="icon-home2 position-left"></i> Home</a></li>
+							<li><a href="form_inputs_basic.html">Forms</a></li>
+							<li class="active">Basic inputs</li>
+						</ul>
+
+						<ul class="breadcrumb-elements">
+							<li><a href="#"><i class="icon-comment-discussion position-left"></i> Support</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+									<i class="icon-gear position-left"></i>
+									Settings
+									<span class="caret"></span>
+								</a>
+
+								<ul class="dropdown-menu dropdown-menu-right">
+									<li><a href="#"><i class="icon-user-lock"></i> Account security</a></li>
+									<li><a href="#"><i class="icon-statistics"></i> Analytics</a></li>
+									<li><a href="#"><i class="icon-accessibility"></i> Accessibility</a></li>
+									<li class="divider"></li>
+									<li><a href="#"><i class="icon-gear"></i> All settings</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- /page header -->
+
+
+				<!-- Content area -->
+				<div class="content">
+
+					<!-- Form horizontal -->
+					<div class="panel panel-flat">
+						<div class="panel-heading">
+							<h5 class="panel-title">Formulario de Establecimientos</h5>
+							<div class="heading-elements">
+								<ul class="icons-list">
+			                		<li><a data-action="collapse"></a></li>
+			                		<li><a data-action="reload"></a></li>
+			                		<li><a data-action="close"></a></li>
+			                	</ul>
+		                	</div>
+						</div>
+
+						<div class="panel-body">
+							<p class="content-group-lg">Este formulario permite gestionar los Establecimientos a utilizar en la generación de los Documentos Fiscales del sistema</p>
+
+							<form class="form-horizontal" action="#">
+								<fieldset class="content-group">
+									<legend class="text-bold">Basic inputs</legend>
+
+									<div class="form-group">
+										<label class="control-label col-lg-2">Id Establecimiento:</label>
+										<div class="col-lg-10">
+											<input type="text" class="form-control" id="idEstablecimiento" name="idEstablecimiento" readonly value="000001">
+										</div>
+									</div>
+
+									
+									<div class="form-group">
+										<label class="control-label col-lg-2">Nombre del Establecimiento:</label>
+										<div class="col-lg-10">
+											<input type="text" class="form-control" id="establecimiento" name="establecimiento">
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label class="control-label col-lg-2">Codigo Fiscal:</label>
+										<div class="col-lg-10">
+											<input type="text" class="form-control" id="establecimiento" name="establecimiento">
+										</div>
+									</div>
+								
+								</fieldset>
+
+								<div class="text-right">
+									<button type="submit" class="btn btn-primary">Submit <i class="icon-arrow-right14 position-right"></i></button>
+								</div>
+							</form>
+						</div>
+					</div>
+					<!-- /form horizontal -->
+
+					
+					<?php include("includes/footer.php"); ?>
+
+				</div>
+				<!-- /content area -->
+
+			</div>
+			<!-- /main content -->
+
+		</div>
+		<!-- /page content -->
+
+	</div>
+	<!-- /page container -->
+
+</body>
+</html>
